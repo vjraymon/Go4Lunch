7d8d512aff5c3cede778a4deda6e5288d267f714
vjraymon <49063544+vjraymon@users.noreply.github.com>
2021-12-28 18:44:24 +0100

    Adds authentifications with email or google

--- app/src/main/java/com/openclassrooms/go4lunch/MainActivity.java
+++ app/src/main/java/com/openclassrooms/go4lunch/MainActivity.java
@@ -1,14 +1,59 @@
 package com.openclassrooms.go4lunch;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startSignInActivity();
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
+
+    private void startSignInActivity() {
+        // Choose authentification providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Launch the activity
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+//                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(providers)
+//                .setIsSmartLockEnabled(false, true)
+//                .setLogo(R.drawable.ic_logo_auth)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            setContentView(R.layout.activity_main);
+        }
+
     }
 }
