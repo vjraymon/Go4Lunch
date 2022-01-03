63f728a26b4ef5611d54ab53163833ae782430a3
vjraymon <49063544+vjraymon@users.noreply.github.com>
2022-01-03 22:30:15 +0100

    Adds authentifications with email or google

--- app/src/main/java/com/openclassrooms/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/openclassrooms/go4lunch/ui/MainActivity.java
@@ -1,28 +1,33 @@
-package com.openclassrooms.go4lunch;
+package com.openclassrooms.go4lunch.ui;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.openclassrooms.go4lunch.R;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private void onClick(View view) {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         startSignInActivity();
     }
 
@@ -35,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
         // Choose authentification providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
 
         // Launch the activity
         Intent signInIntent = AuthUI.getInstance()
@@ -49,11 +55,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
-        IdpResponse response = result.getIdpResponse();
+//      IdpResponse response = result.getIdpResponse();
         if (result.getResultCode() == RESULT_OK) {
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            setContentView(R.layout.activity_main);
+//          FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Button mDisconnectButton = findViewById(R.id.main_disconnect_button);
+
+            extracted(mDisconnectButton);
+        } else {
+            finish();
         }
 
+    }
+
+    private void extracted(Button mDisconnectButton) {
+        mDisconnectButton.setOnClickListener(v -> {
+
+        });
     }
 }
